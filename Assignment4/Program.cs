40c68f6149e60becd3ba290abcde4dd8db924920
visualstudio <visualstudio@122-PC38>
2019-04-10 08:27:50 -0400

    8:27

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,19 @@ namespace Assignment4
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
-              
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+        }
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
+        public void ReadTextFiles()
+        {
+            using(StreamReader file = new StreamReader(")
         }
     }
 }
